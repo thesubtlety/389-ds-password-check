17cab5a6064a83d84899639e30f104b6c41bbdc8
thesubtlety <noah@thesubtlety.com>
2020-02-29 23:49:53 -0800

    remove nss dependency for hashlib

--- 389ds-pwdcheck.py
+++ 389ds-pwdcheck.py
@@ -1,44 +1,30 @@
 #!/usr/bin/env python
+# Author: noah @thesubtlety
 
 from __future__ import absolute_import
 from __future__ import print_function
 
 import sys
+import os
 import base64
 import socket
 import struct
 import argparse
+import hashlib
 
-import nss.nss as nss
-import nss.error as nss_error
-
-pbe_alg     = "SEC_OID_PKCS5_PBKDF2"
-cipher_alg  = "SEC_OID_HMAC_SHA256"
-prf_alg     = "SEC_OID_HMAC_SHA256"
-keylen      = 256
-
-nss.nss_init_nodb()
-
-def b64_hash_from_data(tmp_nss_key, salt, iterations):
+def b64_hash_from_data(tmp_key, salt, iterations):
     iters = int(iterations)
     iterbytes = struct.pack("I", socket.htonl(iters)) 
-    raw = iterbytes + salt + tmp_nss_key
-    buf = base64.b64encode(raw)
+    raw = iterbytes + salt + tmp_key
+    buf = "{PBKDF2_SHA256}" + str(base64.b64encode(raw))
 
     if options.verbose:
         print("Iterations:\n\\x" + "\\x".join(hex(c)[2:] for c in bytearray(iterbytes)) + " (%s)" % iterations)
         print("\nSalt:\n\\x" + "\\x".join(hex(c)[2:] for c in bytearray(salt)))
-        print("\nKey:\n\\x" + "\\x".join(hex(c)[2:] for c in bytearray(tmp_nss_key)))
+        print("\nKey:\n\\x" + "\\x".join(hex(c)[2:] for c in bytearray(tmp_key)))
         print()
 
     return buf
-
-def create_pbkdf2_key(iterations, salt, pwd):
-    alg_id = nss.create_pbev2_algorithm_id(pbe_alg, cipher_alg, prf_alg, keylen, iterations, salt)
-    slot = nss.get_best_slot(0x00000251) # CKM_SHA256_HMAC, same results with 3B0 (CKM_PKCS5_PBKD2)
-    sym_key = slot.pbe_key_gen(alg_id, pwd)
-
-    return sym_key.key_data
 
 def extract_hashinfo(pwdhash_389):
     if pwdhash_389[0] == "{":
@@ -51,36 +37,45 @@ def extract_hashinfo(pwdhash_389):
     assert(iterations < 1000000)
     
     salt = hash_bytes[4:68]
-    nss_key = hash_bytes[68:]
+    target_key = hash_bytes[68:]
     
-    return iterations, salt, nss_key
+    return iterations, salt, target_key
 
 def main():
     global options
     parser = argparse.ArgumentParser(description='Check passwords against Redhat\'s 389-ds pdbkdf2_sha256 hashes')
     parser.add_argument('-v', '--verbose', action='store_true')
     group = parser.add_argument_group('password and hash arguments')
-    group.add_argument('--hash', dest='password_hash', type=str, required=True, help='389-ds formatted password hash {PBKDF2_SHA256}AAnE...')
-    group.add_argument('-p', '--password', type=str, required=True)
+    group.add_argument('--hash', dest='password_hash', type=str, help='389-ds formatted password hash {PBKDF2_SHA256}AAnE...')
+    group.add_argument('-p', '--password', type=str) 
+    group.add_argument('-g', '--generate', dest='generate', help='create a 389-ds password hash')
 
     if len(sys.argv) < 2:
         print(parser.print_help())
         exit(1)
     
-    #password    = 'Password123'
-    #password_hash = "{PBKDF2_SHA256}AAAnEGTxXtnR/oox922/jZyjH6fmiIdW4AwIYZE2LfCVL/SUz5GbAHfjRj4NbN2u8ul0/j/dUzJ4gQSawGALGHZV74nOAtPttoZDTsh7BeGCLD/Ps7vRugwDdz9uPARXzF3bD/8qCpumvRGb4pehzfQsk+FnGgTwi0rUeVaN8a7Kbv8ZpRfU2sd+208F/YL42BWAh/2tv0I4vY7ZsrCZcrUJtgKWy5Nr+t78zmPkrZsX/kgfnGdXhr50kN10cmkLQ0/cZOXo9CAkpeZyFu+wQ5vQdUaES2Vd5kBjJYPCkr4b2ocr4ETQi3IGO2GGCoCetmMIETsudRVSxUNBbva+Vgxin5Apu4wIP/0ZyuGK6TuWLqLnNpmK3RkRx0xjqJ4nN2Ok0ul0XYBJcYIBt4UoaVM2uSa/Etw28Uy+zAsUv2AOiRo5"
     options = parser.parse_args()
 
-    iterations, salt, nss_key = extract_hashinfo(options.password_hash)
-    tmp_nss_key = create_pbkdf2_key(iterations, salt, options.password)
+    keylen = 256
+    if options.generate:
+        pwd =  bytes(str(options.password).encode('utf-8'))
+        salt = bytes(os.urandom(64))
+        iterations = 10000
+        tmp_key = hashlib.pbkdf2_hmac('sha256', pwd, salt, iterations, keylen)
+        print(b64_hash_from_data(tmp_key, salt, iterations))
 
-    if options.verbose:
-        b64_hash_from_data(tmp_nss_key, salt, iterations)
-
-    if nss_key == tmp_nss_key:
-        print("Correct password: {}\n".format(options.password))
     else:
-        print("Incorrect password\n")
+        pwd = bytes(str(options.password).encode('utf-8'))
+        iterations, salt, target_key = extract_hashinfo(options.password_hash)
+        tmp_key = hashlib.pbkdf2_hmac('sha256', pwd, bytes(salt), iterations, keylen)
+
+        if options.verbose:
+            print(b64_hash_from_data(tmp_key, salt, iterations))
+
+        if target_key == tmp_key:
+            print("\nSuccess:\n{}:{}\n".format(options.password_hash,options.password))
+        else:
+            print("Incorrect password\n")
 
 if __name__ == "__main__":
     sys.exit(main())
--- README.md
+++ README.md
@@ -2,27 +2,18 @@
 
 The 389ds-pwdcheck.py script compares passwords against Red Hat's 389-ds `PBKDF2_SHA256` password hashes.
 
-## Requirements
-`pip3 install python-nss`
-
-If python-nss isn't cooperating try `apt install python-nss` or download the source files for nss and nspr and add the header files manually.
-  * https://hg.mozilla.org/projects/nspr
-  * https://hg.mozilla.org/projects/nss
-
-Then
-  * extract the nspr/pr/include/* files to `/usr/local/include/nspr` 
-  * extract the nss/lib/* files to `/usr/local/include/nss`
-
 ## Example Usage
 
 ### Check a password against a password hash
 ```
+
 ~/$ python3 389ds-pwdcheck.py -p Password123 --hash {PBKDF2_SHA256}AAAnEGTxXtnR
 /oox922/jZyjH6fmiIdW4AwIYZE2LfCVL/SUz5GbAHfjRj4NbN2u8ul0/j/dUzJ4gQSawGALGHZV74nOAtPttoZDTsh7BeGCLD/Ps7vRugwDdz9uPARXzF3bD/8qCpumvRGb4pehzfQsk+FnGgT
 wi0rUeVaN8a7Kbv8ZpRfU2sd+208F/YL42BWAh/2tv0I4vY7ZsrCZcrUJtgKWy5Nr+t78zmPkrZsX/kgfnGdXhr50kN10cmkLQ0/cZOXo9CAkpeZyFu+wQ5vQdUaES2Vd5kBjJYPCkr4b2ocr4E
 TQi3IGO2GGCoCetmMIETsudRVSxUNBbva+Vgxin5Apu4wIP/0ZyuGK6TuWLqLnNpmK3RkRx0xjqJ4nN2Ok0ul0XYBJcYIBt4UoaVM2uSa/Etw28Uy+zAsUv2AOiRo5
 
-Correct password: Password123
+Success:
+{PBKDF2_SHA256}AAAnEGTxXtnR/oox922/jZyjH6fmiIdW4AwIYZE2LfCVL/SUz5GbAHfjRj4NbN2u8ul0/j/dUzJ4gQSawGALGHZV74nOAtPttoZDTsh7BeGCLD/Ps7vRugwDdz9uPARXzF3bD/8qCpumvRGb4pehzfQsk+FnGgTwi0rUeVaN8a7Kbv8ZpRfU2sd+208F/YL42BWAh/2tv0I4vY7ZsrCZcrUJtgKWy5Nr+t78zmPkrZsX/kgfnGdXhr50kN10cmkLQ0/cZOXo9CAkpeZyFu+wQ5vQdUaES2Vd5kBjJYPCkr4b2ocr4ETQi3IGO2GGCoCetmMIETsudRVSxUNBbva+Vgxin5Apu4wIP/0ZyuGK6TuWLqLnNpmK3RkRx0xjqJ4nN2Ok0ul0XYBJcYIBt4UoaVM2uSa/Etw28Uy+zAsUv2AOiRo5:Password123
 ```
 
 ### Compare a list of hashes against a wordlist
@@ -40,12 +31,6 @@ Elapsed time: 10.376861095428467 seconds
 ~/$ python ldif-to-hashes.py ./backup.ldif.example backup.ldif.hashes
 username1:{PBKDF2_SHA256}AAAnEGTxXtnR/oox922/jZyjH6fmiIdW4AwIYZE2LfCVL/SUz5GbAHfjRj4NbN2u8ul0/j/dUzJ4gQSawGALGHZV74nOAtPttoZDTsh7BeGCLD/Ps7vRugwDdz9uPARXzF3bD/8qCpumvRGb4pehzfQsk+FnGgTwi0rUeVaN8a7Kbv8ZpRfU2sd+208F/YL42BWAh/2tv0I4vY7ZsrCZcrUJtgKWy5Nr+t78zmPkrZsX/kgfnGdXhr50kN10cmkLQ0/cZOXo9CAkpeZyFu+wQ5vQdUaES2Vd5kBjJYPCkr4b2ocr4ETQi3IGO2GGCoCetmMIETsudRVSxUNBbva+Vgxin5Apu4wIP/0ZyuGK6TuWLqLnNpmK3RkRx0xjqJ4nN2Ok0ul0XYBJcYIBt4UoaVM2uSa/Etw28Uy+zAsUv2AOiRo5
 ```
-
-Heavily based off the python-nss pbkdf2_example.py document at
-https://github.com/tiran/python-nss/blob/master/doc/examples/pbkdf2_example.py
-
-389-ds-base source code and `pbkdf2_pwd.c` files are at
-http://snapshot.debian.org/package/389-ds-base/
 
 More details at https://www.thesubtlety.com/post/a-389-ds-pbkdf2-password-checker/
 
